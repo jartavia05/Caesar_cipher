08224cb5436994effcdfcaf937d3defcf010a278
Jose A. Artavia <jartavia05@gmail.com>
2024-09-10 15:39:18 -0600

    Initial commit

--- brute_force_caesar.py
+++ brute_force_caesar.py
@@ -3,5 +3,5 @@ from caesar_cipher import caesar_cipher, caesar_decipher
 ciphertext = 'JXYJ JX FQUMF GWFAT HTSYFHYFSIT F RNPJ IJQ MTYJQ YFSLT'
 key = 5
 # Podemos intentar esto usando la fuerza bruta, es decir, podemos probar todas las claves posibles y ver cuál tiene más sentido.
-for i in range(1,26):
-    print(caesar_decipher(ciphertext,i))
+for i in range(26):
+    print(f'(key = {i}) {caesar_decipher(ciphertext,i)}')
